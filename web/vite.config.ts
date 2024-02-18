17fb9025589318ef18ad6db6364188af79872ddd
Ripta Pasay <ripta@users.noreply.github.com>
2024-02-18 11:52:17 -0800

    web: move files to root of server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  base: "/one-stack-web/",
+  base: "/",
   build: {
     minify: false,
   },
